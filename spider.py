9a0b5354af9e8d415ad64443048ea24191d20993
makfc <makfc1234@gmail.com>
2018-02-05 20:12:08 +0800

    change log format

--- spider.py
+++ spider.py
@@ -17,10 +17,10 @@ import logging
 import telegram_bot
 
 
-# logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-#                     level=logging.INFO)
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+logging.basicConfig(format='%(asctime)s - %(name)12s - %(levelname)s - %(message)s',
                     level=logging.INFO)
+# logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+#                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 driver = None
